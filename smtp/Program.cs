4ac37880bd3656767a8f03687700e6b42457ee34
Taleh Haciyev <haciyev_99@internet.ru>
2024-09-26 15:20:36 +0400

    smtp
    send mail

---
+++ smtp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+
+var client = new SmtpClient("smtp.gmail.com", 587);
+client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+client.EnableSsl = true;
+
+client.Credentials = new NetworkCredential(userName: "MenSyen", password: "svmf sdfs okgv qipg" ;
+
+var message = new MailMessage()
+{
+    Body = "Hello",
+    Subject = "Test",
+    From = new MailAddress("haciyevtaleh1999@gmail.com", "cry of silence")
+};
+
+message.To.Add("nonameless@gmail.com");
+message.CC.Add("hesenhacizade2@gmail.com");
+
+
+client.Send(message);   
